700236d26a1354e42f0a8dae61278d727403ffeb
tabib-e-alahi <etabib.alahi@gmail.com>
2023-09-01 17:09:41 +0600

    category buttons dynamically added

---
+++ js/categories.js
@@ -0,0 +1,20 @@
+const loadCategoryButtons = async() =>{
+    const res = await fetch('https://openapi.programming-hero.com/api/videos/categories');
+    const data = await res.json();
+    const categories = data.data;
+    // console.log(data.data);
+    displayCategoryButtons(categories);
+}
+
+const displayCategoryButtons = categories =>{
+    const categoryButtonsContainer = document.getElementById('category-buttons-container');
+    
+    categories.forEach(category => {
+        const categoryButton = document.createElement('button');
+        categoryButton.classList.add('btn')
+        categoryButton.innerText = category.category;
+        categoryButtonsContainer.appendChild(categoryButton);
+    } )
+}
+
+loadCategoryButtons()
